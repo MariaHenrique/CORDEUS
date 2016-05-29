a196744d3a96f2d6c695cf26615c3cb7cdcbf1d8
aparecido silvano <silvanoalbuquerque@gmail.com>
2016-05-29 07:28:52 -0300

    Evita duplicata da MainActivity ao Acessar a tela de Sobre.

--- Cordeus/app/src/main/java/empsoft/ufcg/edu/cordeus/views/AboutActivity.java
+++ Cordeus/app/src/main/java/empsoft/ufcg/edu/cordeus/views/AboutActivity.java
@@ -1,9 +1,7 @@
 package empsoft.ufcg.edu.cordeus.views;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 import empsoft.ufcg.edu.cordeus.R;
 
@@ -18,19 +16,4 @@ public class AboutActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                Intent i = new Intent(this, MainActivity.class);
-                startActivity(i);
-                finish();
-                break;
-            default:
-                break;
-        }
-        return true;
-    }
-
 }
